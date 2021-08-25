04afdb427e2b3916a5dfd38910194c57887fb956
ab604 <ab604@soton.ac.uk>
2021-08-25 21:22:48 +0100

    tweak css and cv rmd

--- render_cv.R
+++ render_cv.R
@@ -4,7 +4,7 @@
 # this script to cache a version of the CV_Printer class with data already
 # loaded and load the cached version in the .Rmd instead of re-fetching it twice
 # for the HTML and PDF rendering. This exercise is left to the reader.
-googlesheets4::gs4_deauth()
+
 # Knit the HTML version
 rmarkdown::render("cv.rmd",
                   params = list(pdf_mode = FALSE),
